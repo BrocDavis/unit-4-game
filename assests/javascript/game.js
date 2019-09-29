97350e308b9f8f0f582abd10b5fa2d6fe54b1c52
Broc <broc.davis@yahoo.com>
2019-09-29 18:19:52 -0500

    removed console logs from click functions and made a winCheck function to check everytime a crystal is clicked. tested functionality for wins and losses to make sure game reset for each case of the game

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -4,52 +4,59 @@ var currentTotal = 0;
 var goalNum = 0;
 var cyrstalNum = [];
 
-// $(document).ready(function() {
+$(document).ready(function() {
+//on page load to set values
+reset();
 
+$(".green-rupee").click(function () {
+    currentTotal += 1;
+    winCheck();
+});
+$(".red-rupee").click(function () {
+    currentTotal += 2;
+    winCheck();
+});
+$(".blue-rupee").click(function () {
+    currentTotal += 3;
+    winCheck();
+});
+$(".purple-rupee").click(function () {
+    currentTotal += 4;
+    winCheck();
+});
 
-// });
+    function winCheck(){
+        if (currentTotal > goalNum) {
+            losses++;
+            reset();
+        }
+        else if (currentTotal === goalNum) {
+            wins++;
+            reset();
+        }
+        updateDisplay();
+    }
 
+//setting goalNum to random number between 19-120.
 function reset() {
-    //setting goalNum to random number between 19-120.
     $(".total-number").text(goalNum = setGoalNum(19, 120));
     $(".current-total").text(currentTotal = 0);
     $(".loss-count").text(losses);
     $(".win-count").text(wins);
 }
+
 //sets goalNum to random number  
 function setGoalNum(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+//updates display
 function updateDisplay(){
     $(".current-total").text(currentTotal);
+    $(".loss-count").text(losses);
+    $(".win-count").text(wins);
 }
 
-$(".green-rupee").click(function () {
-    currentTotal += 1;
-    updateDisplay();
-});
-$(".red-rupee").click(function () {
-    currentTotal += 2;
-    updateDisplay();``
-});
-$(".blue-rupee").click(function () {
-    currentTotal += 3;
-    updateDisplay();
-});
-$(".purple-rupee").click(function () {
-    currentTotal += 4;
-    updateDisplay();
-});
-
-
-if (currentTotal > goalNum) {
-    losses++;
-}
-else if(currentTotal < goalNum){
-    // updateDisplay();
-}
-
-//game start
-reset();
+});// end of document.ready
