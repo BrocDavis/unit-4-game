53ef542d3fe5a8131631ff40f42431fbbce3f217
Broc <broc.davis@yahoo.com>
2019-09-29 15:10:34 -0500

    added classes for jqery references to updated values like wines losses and the goal number and current total number. added in reset function to have the game set and reset after first play through and added in the random function for the goal number

---
+++ assests/javascript/game.js
@@ -0,0 +1,22 @@
+var wins = 0;
+var losses = 0;
+var currentTotal = 0;
+var goalNum = 0;
+
+
+// $(document).ready(function() {
+ 
+ 
+// });
+
+function reset(){
+    //setting goalNum to random number between 19 -120.
+    goalNum = setGoalNum(19,120);
+    currentTotal = 0;
+}
+  //sets goalNum to random number  
+function setGoalNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
