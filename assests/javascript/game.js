a6b35cbd3772a455760ae6be11a896e16d63f310
Broc <broc.davis@yahoo.com>
2019-09-29 19:08:23 -0500

    formatted game.js and removed redundant lines of code from updateDisplay for loss and win numbers since they only need to be updated on win or loss condition not every update.

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -4,28 +4,28 @@ var currentTotal = 0;
 var goalNum = 0;
 var crystalNum = [];
 
-$(document).ready(function() {
-//on page load to set values
-reset();
+$(document).ready(function () {
+    //on page load to set values
+    reset();
 
-$(".green-rupee").click(function () {
-    currentTotal += crystalNum[0];
-    winCheck();
-});
-$(".red-rupee").click(function () {
-    currentTotal += crystalNum[1];
-    winCheck();
-});
-$(".blue-rupee").click(function () {
-    currentTotal += crystalNum[2];
-    winCheck();
-});
-$(".purple-rupee").click(function () {
-    currentTotal += crystalNum[3];
-    winCheck();
-});
+    $(".green-rupee").click(function () {
+        currentTotal += crystalNum[0];
+        winCheck();
+    });
+    $(".red-rupee").click(function () {
+        currentTotal += crystalNum[1];
+        winCheck();
+    });
+    $(".blue-rupee").click(function () {
+        currentTotal += crystalNum[2];
+        winCheck();
+    });
+    $(".purple-rupee").click(function () {
+        currentTotal += crystalNum[3];
+        winCheck();
+    });
 
-    function winCheck(){
+    function winCheck() {
         if (currentTotal > goalNum) {
             losses++;
             reset();
@@ -37,30 +37,28 @@ $(".purple-rupee").click(function () {
         updateDisplay();
     }
 
-//setting goalNum to random number between 19-120.
-function reset() {
-    $(".total-number").text(goalNum = setRandomNum(19, 120));
-    crystalNum[0] = setRandomNum(1,12);
-    crystalNum[1] = setRandomNum(1,12);
-    crystalNum[2] = setRandomNum(1,12);
-    crystalNum[3] = setRandomNum(1,12);
-    $(".current-total").text(currentTotal = 0);
-    $(".loss-count").text(losses);
-    $(".win-count").text(wins);
-}
+    //setting goalNum to random number between 19-120.
+    function reset() {
+        $(".total-number").text(goalNum = setRandomNum(19, 120));
+        crystalNum[0] = setRandomNum(1, 12);
+        crystalNum[1] = setRandomNum(1, 12);
+        crystalNum[2] = setRandomNum(1, 12);
+        crystalNum[3] = setRandomNum(1, 12);
+        $(".current-total").text(currentTotal = 0);
+        $(".loss-count").text(losses);
+        $(".win-count").text(wins);
+    }
 
-//sets goalNum to random number with specified min and max values
-function setRandomNum(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+    //sets goalNum to random number with specified min and max values
+    function setRandomNum(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-//updates display
-function updateDisplay(){
-    $(".current-total").text(currentTotal);
-    $(".loss-count").text(losses);
-    $(".win-count").text(wins);
-}
+    //updates display
+    function updateDisplay() {
+        $(".current-total").text(currentTotal);
+    }
 
 });// end of document.ready
